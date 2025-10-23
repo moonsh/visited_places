26389c64110692f9c10b7738118b107080911ab5
Seonghyeon Moon <msh8912@gmail.com>
2025-10-22 23:31:44 -0700

    update
    update

--- cities.js
+++ cities.js
@@ -3,5 +3,28 @@ CITIES =
    // {name: 'Venice', latitude: 45.4408, longitude: 12.3155, radius: 3, fillKey: 'city', date: '1963-10'},
    // Not really a city, but we define just one type of "POI" (fillKey here) in index.html, you can define more.
    // {name: 'Khao Phing Kan', latitude: 8.2745, longitude: 98.5012, radius: 3, fillKey: 'city', date: '1974-12'},
-   {name: 'San Francisco', latitude: 37.7749, longitude: -122.4194, radius: 3, fillKey: 'city', date: '2023-05'},   
+{name: 'San Francisco', latitude: 37.7749, longitude: -122.4194, radius: 3, fillKey: 'city', date: 'Current'},   
+{name: 'Anchorage', latitude: 61.2181, longitude: -149.9003, radius: 3, fillKey: 'city', date: '2025 (ICIP)'},
+{name: 'Fairbanks', latitude: 64.8378, longitude: -147.7164, radius: 3, fillKey: 'city', date: '2025'},
+{name: 'Toronto', latitude: 43.6511, longitude: -79.3839, radius: 3, fillKey: 'city', date: '2019 (First Internship)'},
+{name: 'Piscataway', latitude: 40.5511, longitude: -74.4637, radius: 3, fillKey: 'city', date: '2024 (Ph.D.)'},
+{name: 'Coram', latitude: 40.8687, longitude: -73.0015, radius: 3, fillKey: 'city', date: '2025 (PostDoc)'},
+{name: 'Cancun', latitude: 21.1619, longitude: -86.8515, radius: 3, fillKey: 'city', date: '2023'},
+{name: 'Paris', latitude: 48.8566, longitude: 2.3522, radius: 3, fillKey: 'city', date: '2023 (ICCV)'},
+{name: 'London', latitude: 51.5074, longitude: -0.1278, radius: 3, fillKey: 'city', date: '2023'},
+{name: 'Chicago', latitude: 41.8781, longitude: -87.6298, radius: 3, fillKey: 'city', date: '2025'},
+{name: 'Daejeon', latitude: 36.3504, longitude: 127.3845, radius: 3, fillKey: 'city', date: 'Born'},
+{name: 'Seoul', latitude: 37.5665, longitude: 126.9780, radius: 3, fillKey: 'city', date: '2009 (B.S.)'},
+{name: 'Gwangju', latitude: 35.1595, longitude: 126.8526, radius: 3, fillKey: 'city', date: '2015 (M.S.)'},
+{name: 'Tel Aviv', latitude: 32.0853, longitude: 34.7818, radius: 3, fillKey: 'city', date: '2022 (ECCV)'},
+
+
+
+
+
+
+
+
+
+
 ]
