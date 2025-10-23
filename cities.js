873d20fe56b58de0fb7ea749b04019247d191adc
Seonghyeon Moon <msh8912@gmail.com>
2025-10-22 23:46:52 -0700

    Update cities.js

--- cities.js
+++ cities.js
@@ -7,16 +7,30 @@ CITIES =
 {name: 'Anchorage', latitude: 61.2181, longitude: -149.9003, radius: 3, fillKey: 'city', date: '2025 (ICIP)'},
 {name: 'Fairbanks', latitude: 64.8378, longitude: -147.7164, radius: 3, fillKey: 'city', date: '2025'},
 {name: 'Toronto', latitude: 43.6511, longitude: -79.3839, radius: 3, fillKey: 'city', date: '2019 (First Internship)'},
-{name: 'Piscataway', latitude: 40.5511, longitude: -74.4637, radius: 3, fillKey: 'city', date: '2024 (Ph.D.)'},
+{name: 'Piscataway', latitude: 40.5511, longitude: -74.4637, radius: 3, fillKey: 'city', date: '2024 (Ph.D)'},
 {name: 'Coram', latitude: 40.8687, longitude: -73.0015, radius: 3, fillKey: 'city', date: '2025 (PostDoc)'},
 {name: 'Cancun', latitude: 21.1619, longitude: -86.8515, radius: 3, fillKey: 'city', date: '2023'},
 {name: 'Paris', latitude: 48.8566, longitude: 2.3522, radius: 3, fillKey: 'city', date: '2023 (ICCV)'},
 {name: 'London', latitude: 51.5074, longitude: -0.1278, radius: 3, fillKey: 'city', date: '2023'},
 {name: 'Chicago', latitude: 41.8781, longitude: -87.6298, radius: 3, fillKey: 'city', date: '2025'},
 {name: 'Daejeon', latitude: 36.3504, longitude: 127.3845, radius: 3, fillKey: 'city', date: 'Born'},
-{name: 'Seoul', latitude: 37.5665, longitude: 126.9780, radius: 3, fillKey: 'city', date: '2009 (B.S.)'},
-{name: 'Gwangju', latitude: 35.1595, longitude: 126.8526, radius: 3, fillKey: 'city', date: '2015 (M.S.)'},
+{name: 'Seoul', latitude: 37.5665, longitude: 126.9780, radius: 3, fillKey: 'city', date: '2009 (B.S)'},
+{name: 'Gwangju', latitude: 35.1595, longitude: 126.8526, radius: 3, fillKey: 'city', date: '2015 (M.S)'},
 {name: 'Tel Aviv', latitude: 32.0853, longitude: 34.7818, radius: 3, fillKey: 'city', date: '2022 (ECCV)'},
+{name: 'Pilsen', latitude: 49.7465, longitude: 13.3776, radius: 3, fillKey: 'city', date: '2017 (WSCG)'},
+{name: 'Newcastle', latitude: 54.9783, longitude: -1.6178, radius: 3, fillKey: 'city', date: '2013 (Northumbria Uni)'},
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
